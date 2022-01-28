2857cdf04bf37b7a2fc5754e619ebb74ef9f83ab
David Blane <david.blane@digicatapult.org.uk>
2022-01-28 14:42:05 +0000

    Initial gh-pages commit

--- test/mocharc-unit.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  recursive: true,
-  slow: 1000,
-  timeout: 2000,
-  extension: ['.test.js'],
-  exit: true,
-}
--- test/mocharc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  recursive: true,
-  slow: 10000,
-  timeout: 20000,
-  extension: ['.test.js'],
-  require: ['./test/integration/fixtures.js'],
-  exit: true,
-}
