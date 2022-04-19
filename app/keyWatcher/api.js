dec9574973b608d9e9552d8dc91099505aa15643
Jonathan Gray <jonmattgray@gmail.com>
2022-04-19 12:11:25 +0100

    Use dscp-node types package (#24)
    * use types package
    
    * use latest node
    
    * fix envalid version
    
    * use configurable api package
    
    * keyring
    
    * remove logger in buildApi
    
    * use ^ for node, revert envalid

--- app/keyWatcher/api.js
+++ app/keyWatcher/api.js
@@ -1,4 +1,5 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api')
+const { buildApi } = require('@digicatapult/dscp-node')
+
 const {
   NODE_HOST,
   NODE_PORT,
@@ -7,81 +8,22 @@ const {
   PROCESS_IDENTIFIER_LENGTH,
 } = require('../env')
 
-const provider = new WsProvider(`ws://${NODE_HOST}:${NODE_PORT}`)
-const apiOptions = {
-  provider,
-  types: {
-    Address: 'MultiAddress',
-    LookupSource: 'MultiAddress',
-    PeerId: 'Vec<u8>',
-    Key: 'Vec<u8>',
-    TokenId: 'u128',
-    RoleKey: 'Role',
-    TokenMetadataKey: `[u8; ${METADATA_KEY_LENGTH}]`,
-    TokenMetadataValue: 'MetadataValue',
-    Token: {
-      id: 'TokenId',
-      original_id: 'TokenId',
-      roles: 'BTreeMap<RoleKey, AccountId>',
-      creator: 'AccountId',
-      created_at: 'BlockNumber',
-      destroyed_at: 'Option<BlockNumber>',
-      metadata: 'BTreeMap<TokenMetadataKey, TokenMetadataValue>',
-      parents: 'Vec<TokenId>',
-      children: 'Option<Vec<TokenId>>',
-    },
-    ProcessIO: {
-      roles: 'BTreeMap<RoleKey, AccountId>',
-      metadata: 'BTreeMap<TokenMetadataKey, TokenMetadataValue>',
-      parent_index: 'Option<u32>',
-    },
-    MetadataValue: {
-      _enum: {
-        File: 'Hash',
-        Literal: `[u8; ${METADATA_VALUE_LITERAL_LENGTH}]`,
-        TokenId: 'TokenId',
-        None: 'null',
-      },
-    },
-    Role: {
-      _enum: ['Owner', 'Customer', 'AdditiveManufacturer', 'Laboratory', 'Buyer', 'Supplier', 'Reviewer'],
-    },
-    ProcessIdentifier: `[u8; ${PROCESS_IDENTIFIER_LENGTH}]`,
-    ProcessVersion: 'u32',
-    ProcessId: {
-      id: 'ProcessIdentifier',
-      version: 'ProcessVersion',
-    },
-    Process: {
-      status: 'ProcessStatus',
-      restrictions: 'Vec<Restriction>',
-    },
-    ProcessStatus: {
-      _enum: ['Disabled', 'Enabled'],
-    },
-    Restriction: {
-      _enum: {
-        None: '()',
-        SenderOwnsAllInputs: '()',
-        FixedNumberOfInputs: 'FixedNumberOfInputsRestriction',
-        FixedNumberOfOutputs: 'FixedNumberOfOutputsRestriction',
-      },
-    },
-    FixedNumberOfInputsRestriction: {
-      num_inputs: 'u32',
-    },
-    FixedNumberOfOutputsRestriction: {
-      num_outputs: 'u32',
-    },
-    IsNew: 'bool',
-    Restrictions: 'Vec<Restriction>',
-  },
-}
-
 const createNodeApi = async () => {
-  const api = await ApiPromise.create(apiOptions)
+  const { api, keyring } = buildApi({
+    options: {
+      apiHost: NODE_HOST,
+      apiPort: NODE_PORT,
+      metadataKeyLength: METADATA_KEY_LENGTH,
+      metadataValueLiteralLength: METADATA_VALUE_LITERAL_LENGTH,
+      processorIdentifierLength: PROCESS_IDENTIFIER_LENGTH,
+    },
+  })
+
+  await api.isReady
+
   return {
     _api: api,
+    _keyring: keyring,
     isEventKeyUpdate: (event) => api.events.ipfsKey.UpdateKey.is(event),
     getCurrentKey: async () => await api.query.ipfsKey.key(),
     setupEventProcessor: (eventProcessor) => api.query.system.events(eventProcessor),
